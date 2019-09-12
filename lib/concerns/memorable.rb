bd53ce4da88a3e2d8cb0ede5f45c8955fe5f3e5e
Luis Herrera <herrera.lrh@gmail.com>
2019-09-12 13:12:30 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,2 +1,17 @@
 module memorable
+  module ClassMethods
+    def reset_all
+      self.all.clear
+    end
+
+    def count
+      self.all.count
+    end
+  end
+
+  module InstanceMethods
+    def initialize
+      self.class.all << self
+    end
+  end
 end
